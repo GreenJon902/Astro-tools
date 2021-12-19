11bdf57a1cbebba55ae91493299d257a800953a2
GreenJon902 <GreenJon902@gmail.com>
2021-12-19 20:00:06 +0000

    added docstring and upadted arg + return

--- Scripts/noiseReduction.py
+++ Scripts/noiseReduction.py
@@ -3,8 +3,14 @@ from typing import Union, IO, Optional
 import numpy as np
 
 
-def remove_noise(img_in: Union[str, IO, np.ndarray], img_out: Union[str, IO] = None) -> np.ndarray:
-    pass
+def remove_noise(img_in: Union[str, IO, np.ndarray], img_out: Union[str, IO] = None) -> Optional[np.ndarray]:
+    """
+    A function to remove noise from an image
+
+    :param img_in: The image that will have noise removed from
+    :param img_out: The final image (none if you just want the np.ndarray and don't want to write to a file)
+    :return:
+    """
 
 
 if __name__ == '__main__':
